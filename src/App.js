fe5f580a1c071383d345f954bb05cc180c41c13e
Alex Norton <atn362@gmail.com>
2022-04-17 10:10:45 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./opus.svg";
+import logo from "./Kermit.png";
 import "./App.css";
 
 function App() {
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Happy Easter Little Raven!!!</p>
       </header>
     </div>
   );
